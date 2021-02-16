5dddcc2a3b7186a79a8acd4d3fa0d56aab0cdfb8
DanielAubin <DanielAubin@users.noreply.github.com>
2021-02-16 11:38:26 +0100

    Update R-learner.R

--- R-learner/R-learner.R
+++ R-learner/R-learner.R
@@ -31,7 +31,16 @@ R_learner <- function(df_aux,df_main,covariates,learners){
     df_aux1<- as.data.frame(df_aux[ ind,])
     df_aux2 <- as.data.frame(df_aux[-ind,])
 
-m_mod <- SuperLearner(Y = df_aux1$y, X = df_aux1[,covariates], newX = df_aux2[,covariates], SL.library = learners,
+       # Train a classification model to get the propensity scores
+    p_mod <- SuperLearner(Y = df_aux1$d, X = df_aux1[,covariates], newX = df_aux2[,covariates], SL.library = learners,
+                          verbose = FALSE, method = "method.NNLS", family = binomial(),cvControl = control)
+    
+    p_hat <- p_mod$SL.predict
+    p_hat = ifelse(p_hat<0.025, 0.025, ifelse(p_hat>.975,.975, p_hat)) # Overlap bounding
+    
+      
+      
+   m_mod <- SuperLearner(Y = df_aux1$y, X = df_aux1[,covariates], newX = df_aux2[,covariates], SL.library = learners,
                         verbose = FALSE, method = "method.NNLS",cvControl = control)
   
   m_hat <- m_mod$SL.predict
