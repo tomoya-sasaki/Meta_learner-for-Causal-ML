a8f1e98b0b82cc16eaa36aa545404b0e082ea4bc
DanielAubin <DanielAubin@users.noreply.github.com>
2021-02-16 11:37:15 +0100

    Update X-learner.R

--- X-learner/X-learner.R
+++ X-learner/X-learner.R
@@ -15,23 +15,33 @@ control <- SuperLearner.CV.control(V=5)
 
 X_learner <- function(df_aux,df_main,covariates,learners){
 
+  est_cate_X <- matrix(0,nrow(df_main),R)
   
-  p_mod <- SuperLearner(Y = df_aux$d, X = df_aux[,covariates], newX = df_aux[,covariates], SL.library = learners,
+  
+  
+    for(r in 1:R){
+    
+    ind <- createDataPartition(df_aux[,"d"], p = .5, list = FALSE)
+    
+    df_aux1<- as.data.frame(df_aux[ ind,])
+    df_aux2 <- as.data.frame(df_aux[-ind,])
+  
+  p_mod <- SuperLearner(Y = df_aux$d, X = df_aux1[,covariates], newX = df_main[,covariates], SL.library = learners,
                         verbose = FALSE, method = "method.NNLS", family = binomial(),cvControl = control)
   
   # Prop-Score for df_main
-  p_hat_main <- predict(p_mod,df_main[,covariates])$pred
+  p_hat_main <- p_mod$predictions
   p_hat_main = ifelse(p_hat<0.025, 0.025, ifelse(p_hat>.975,.975, p_hat)) # Overlap bounding
   
-  aux_1 <- df_aux[which(df_aux$d==1),]
-  aux_0 <- df_aux[which(df_aux$d==0),]
+  aux_1 <- df_aux1[which(df_aux1$d==1),]
+  aux_0 <- df_aux1[which(df_aux1$d==0),]
   
-  m1_mod <- SuperLearner(Y = aux_1$y, X = aux_1[,covariates], newX = df_aux[,covariates], SL.library = learners,
+  m1_mod <- SuperLearner(Y = aux_1$y, X = aux_1[,covariates], newX = df_aux2[,covariates], SL.library = learners,
                          verbose = FALSE, method = "method.NNLS",cvControl = control)
   
   m1_hat <- m1_mod$SL.predict
   
-  m0_mod <- SuperLearner(Y = aux_0$y, X = aux_0[,covariates], newX = df_aux[,covariates], SL.library = learners,
+  m0_mod <- SuperLearner(Y = aux_0$y, X = aux_0[,covariates], newX = df_aux2[,covariates], SL.library = learners,
                          verbose = FALSE, method = "method.NNLS",cvControl = control)
   
   m0_hat <- m0_mod$SL.predict
@@ -39,13 +49,13 @@ X_learner <- function(df_aux,df_main,covariates,learners){
   
   
   
-  tau1 <- df_aux[which(df_aux$d==1),"y"] - m0_hat[which(df_aux$d==1),]
+  tau1 <- df_aux2[which(df_aux2$d==1),"y"] - m0_hat[which(df_aux2$d==1),]
   
-  tau0 <- m1_hat[which(df_aux$d==0),]  -  df_aux[which(df_aux$d==0),"y"]
+  tau0 <- m1_hat[which(df_aux2$d==0),]  -  df_aux2[which(df_aux2$d==0),"y"]
   
   
   a1  <- tryCatch({
-    tau1_mod <- SuperLearner(Y = tau1, X = df_aux[which(df_aux$d==1),covariates], newX = df_main[,covariates], SL.library = learners,
+    tau1_mod <- SuperLearner(Y = tau1, X = df_aux2[which(df_aux2$d==1),covariates], newX = df_main[,covariates], SL.library = learners,
                              verbose = FALSE, method = "method.NNLS",cvControl = control)
     
     score_tau1 <- tau1_mod$SL.predict
@@ -55,7 +65,7 @@ X_learner <- function(df_aux,df_main,covariates,learners){
   },error=function(e){
     
     mean_score <- mean(tau1)
-    score_tau1 <- rep.int(mean_score, times = nrow(df_aux))
+    score_tau1 <- rep.int(mean_score, times = nrow(df_main))
     a1 <- score_tau1
     return(a1)
   })
@@ -63,7 +73,7 @@ X_learner <- function(df_aux,df_main,covariates,learners){
   score_tau1 <- a1
   
   a0  <- tryCatch({
-    tau0_mod <- SuperLearner(Y =tau0, X = df_aux[which(df_aux$d==0),covariates], newX = df_main[,covariates], SL.library = learners,
+    tau0_mod <- SuperLearner(Y =tau0, X = df_aux2[which(df_aux2$d==0),covariates], newX = df_main[,covariates], SL.library = learners,
                              verbose = FALSE, method = "method.NNLS",cvControl = control)
     
     score_tau0 <- tau0_mod$SL.predict
@@ -73,7 +83,7 @@ X_learner <- function(df_aux,df_main,covariates,learners){
   },error=function(e){
     
     mean_score <- mean(tau0)
-    score_tau0 <- rep.int(mean_score, times = nrow(df_aux))
+    score_tau0 <- rep.int(mean_score, times = nrow(df_main))
     a0 <- score_tau0
     return(a0)
   })
@@ -82,9 +92,11 @@ X_learner <- function(df_aux,df_main,covariates,learners){
   
   
   
-  tau_weight <- p_hat*score_tau0 + (1-p_hat)*score_tau1
+  tau_weight <- p_hat_main*score_tau0 + (1-p_hat_main)*score_tau1
   
   
+    est_cate_X[,r] <- score_dr 
+  }
   
-  return(tau_weight)
+  return(apply(est_cate_X,1,median))
   }
