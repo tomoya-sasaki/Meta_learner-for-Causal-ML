7bf70fbae4749d2103027452799a786072000f8f
DanielAubin <DanielAubin@users.noreply.github.com>
2021-04-15 11:19:54 +0200

    Update Bootstrap_CI.R

--- Confidence_Intervals/Bootstrap_CI.R
+++ Confidence_Intervals/Bootstrap_CI.R
@@ -1,5 +1,5 @@
 
-
+# This function used the mean of the bootstrapped estimates as the point of interest (pred=).
 bootCI <- function(pred_B){
   
 
@@ -19,3 +19,24 @@ bootCI <- function(pred_B){
   ))
 }
 
+# An alternative is to not use a bootstrapped version for the point of interest.
+# This means we estimate the CATE based on sample-splitting and use these estimates as the prediction.
+                 
+bootCI <- function(pred_B,score){
+  
+  
+  # the the 5% and 95% CI from the bootstrapped procedure
+  CI_b <- data.frame(
+    X5. =  apply(pred_B, 1, function(x)
+      quantile(x, c(.025))),
+    X95. = apply(pred_B, 1, function(x)
+      quantile(x, c(.975))),
+    sd = apply(pred_B, 1, function(x) sd(x))
+  )
+  
+  return(data.frame(
+    pred = score,
+    X5. =  score - 1.96 * CI_b$sd,
+    X95. = score + 1.96 * CI_b$sd
+  ))
+}
