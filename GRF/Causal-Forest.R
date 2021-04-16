2e965c99a4ccbc0470c6d46a32ef182dfea9e1d4
DanielAubin <DanielAubin@users.noreply.github.com>
2021-04-16 11:21:27 +0200

    Update Causal-Forest.R

--- GRF/Causal-Forest.R
+++ GRF/Causal-Forest.R
@@ -1,7 +1,42 @@
 
-Causal_Forest <- function(df_aux,df_main,covariates,learners){
+Causal_Forest <- function(data,covariates,learners,control){
   
+  data$ID <- c(1:nrow(data))
 
+pseudo_all <- matrix(NA,nrow(data),2)
+ID_pseudo <- 1:nrow(data)
+pseudo_all <- cbind(pseudo_all,ID_pseudo)
+  
+  folds <- createFolds(data$d,k=5)
+
+for(f in 1:(length(folds))){
+  
+  if(f == 1){
+    data1 <- data[c(folds[[5]],folds[[2]],folds[[3]],folds[[4]]),]
+    df_main <- data[folds[[1]],]
+  } 
+  if(f == 2){
+    data1 <- data[c(folds[[1]],folds[[5]],folds[[3]],folds[[4]]),]
+    df_main <- data[folds[[2]],]
+  } 
+  
+  if(f == 3){
+    data1 <- data[c(folds[[1]],folds[[2]],folds[[5]],folds[[4]]),]
+    df_main <- data[folds[[3]],]
+  } 
+  
+  if(f == 4){
+    data1 <- data[c(folds[[1]],folds[[2]],folds[[3]],folds[[5]]),]
+    df_main <- data[folds[[4]],]
+  } 
+  
+  if(f == 5){
+    data1 <- data[c(folds[[1]],folds[[2]],folds[[3]],folds[[4]]),]
+    df_main <- data[folds[[5]],]
+  } 
+  
+df_aux <- data1
+  
   p_mod <- SuperLearner(Y = df_aux$d, X = df_aux[,covariates], newX = df_main[,covariates], SL.library = learners,
                         verbose = FALSE, method = "method.NNLS", family = binomial(),cvControl = control)
   
@@ -14,14 +49,21 @@ Causal_Forest <- function(df_aux,df_main,covariates,learners){
                          verbose = FALSE, method = "method.NNLS",cvControl = control)
 
   m_hat <- m_mod$SL.predict
+  
+  ## Collect all nuisance parameters
+  pseudo_all[,1][df_main$ID] <- m_hat
+  pseudo_all[,2][df_main$ID] <- p_hat
+  
+ } 
 
 
-tau.forest <- causal_forest(df_main[,covariates], df_main$y, df_main$d,
-                           W.hat = p_hat, Y.hat = m_hat,
+
+tau.forest <- causal_forest(data[,covariates], data$y, d$d,
+                           W.hat = pseudo_all[,2], Y.hat = pseudo_all[,1],
                            tune.parameters = "all")
 
 
-tau.hat <- predict(tau.forest, df_main[,covariates])
+tau.hat <- predict(tau.forest, data[,covariates])
 
 return(tau.hat$predictions)
 
